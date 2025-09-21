5077393861d04a5989bb3f686210620a58ae7b9b
Luis Chang <luis.chang@outlook.com>
2025-09-20 20:03:52 -0500

    Initial commit

---
+++ app/src/main/java/dev/luischang/appdpa/MainActivity.kt
@@ -0,0 +1,32 @@
+package dev.luischang.appdpa
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dev.luischang.appdpa.presentation.auth.LoginScreen
+import dev.luischang.appdpa.presentation.auth.RegisterScreen
+import dev.luischang.appdpa.ui.theme.AppDPATheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AppDPATheme {
+                //LoginScreen()
+                RegisterScreen()
+            }
+        }
+    }
+}
+
+
+
---
+++ app/src/main/java/dev/luischang/appdpa/presentation/auth/RegisterScreen.kt
@@ -0,0 +1,73 @@
+package dev.luischang.appdpa.presentation.auth
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun RegisterScreen(){
+
+    var name by remember {mutableStateOf("")}
+    var email by remember {mutableStateOf("")}
+    var password by remember {mutableStateOf("")}
+    var confirmPassword by remember {mutableStateOf("")}
+
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ){
+        Spacer(modifier = Modifier.padding(16.dp))
+        Text("Registro de Usuario", style = MaterialTheme.typography.titleLarge)
+
+        OutlinedTextField(
+            value = name,
+            onValueChange = { name = it },
+            label = { Text("Nombre") },
+            modifier = Modifier.padding(vertical = 8.dp).fillMaxWidth()
+        )
+        //OutlinedTextField for email
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Correo electrónico") },
+            modifier = Modifier.padding(vertical = 8.dp).fillMaxWidth()
+        )
+        //OutlinedTextField for password
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Contraseña") },
+            modifier = Modifier.padding(vertical = 8.dp).fillMaxWidth(),
+            visualTransformation = PasswordVisualTransformation()
+        )
+        //OutlinedTextField for confirm password
+        OutlinedTextField(
+            value = confirmPassword,
+            onValueChange = { confirmPassword = it },
+            label = { Text("Confirmar contraseña") },
+            modifier = Modifier.padding(vertical = 8.dp).fillMaxWidth(),
+            visualTransformation = PasswordVisualTransformation()
+        )
+        //Button for register
+        Button(
+            onClick = { /* Handle register button click */ },
+            modifier = Modifier.padding(vertical = 8.dp).fillMaxWidth()
+        ) {
+            Text("Registrar")
+        }
+
+    }
+
+}
